89da24141766be6f45046e6264675f0daf544857
TariqHattab <67665495+TariqHattab@users.noreply.github.com>
2021-04-21 22:27:15 +0300

    commit 3

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,14 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          child: Text('this works!!'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/Rt3cI3uj8g1gWu6bL39m/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          var documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/Rt3cI3uj8g1gWu6bL39m/messages')
+              .add({'text': 'this another text'});
+        },
       ),
     );
   }
